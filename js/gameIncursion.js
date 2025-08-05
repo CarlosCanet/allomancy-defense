b51d149d06375bc0defb14583e132875c0bbef39
Carlos Canet <carlos@carlosca.net>
2025-08-05 17:59:06 +0200

    feat: add static buildings to incursion scene

--- js/gameIncursion.js
+++ js/gameIncursion.js
@@ -1,4 +1,5 @@
-import { RESOURCES } from "./game.js";
+import { Building } from "./building.js";
+import { METALS_RESOURCES, RESOURCES } from "./game.js";
 import { MenuSection } from "./gameIdle.js";
 export class GameIncursion {
     gameBoxNode;
@@ -39,5 +40,27 @@ export class GameIncursion {
             this.resources.set(resource, 0);
             this.resourcesMenuSectionNode.addElement(resource, resource);
         });
+        this.createBuildings();
     };
+    createBuildings = () => {
+        const varX = 150, varY = 50, varSize = 0.5;
+        for (let i = 0; i < 5; i++) {
+            for (let j = 0; j < 3; j++) {
+                let x = 150;
+                let y = 80;
+                let w = 100;
+                let h = 100;
+                let dx = Math.floor(Math.random() * varX - varX / 2);
+                let dy = Math.floor(Math.random() * varY - varY / 2);
+                let ds = (Math.random() * varSize - varSize / 3) + 1;
+                let newBuilding = new Building(x + i * 350 + dx, y + j * 200 + dy, w * ds, h * ds, document.createElement("div"), "");
+                newBuilding.node.style.width = `${newBuilding.w}px`;
+                newBuilding.node.style.height = `${newBuilding.h}px`;
+                newBuilding.node.classList.add("static-buildings");
+                newBuilding.render();
+                this.baseNode.append(newBuilding.node);
+            }
+        }
+    };
+    gameLoop() { }
 }
